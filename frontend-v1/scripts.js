417b0b91f930c33021c2179aa262cf78200ab784
Tulio Faria <tuliofaria@gmail.com>
2022-05-20 17:43:56 -0300

    feat(frontend-v1): allow discovery of baseUrl based on the hostname

--- frontend-v1/scripts.js
+++ frontend-v1/scripts.js
@@ -1,4 +1,9 @@
-const baseUrl = 'http://localhost:3000'
+const baseUrl =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:3000'
+    : 'https://https://commenting-test.herokuapp.com'
+
+console.log('using ', baseUrl, 'as endpoint.')
 
 document.addEventListener('DOMContentLoaded', function () {
   Comments.loadAndRenderComments()
